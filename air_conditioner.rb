b328ec764db766b0d2af559f9cf32c5f468e2b92
ogmegumi <ogmegumi@gmail.com>
2021-10-08 12:11:57 +0000

    add:air_conditionar

--- air_conditioner.rb
+++ air_conditioner.rb
@@ -1,4 +1,4 @@
-input_lines =readlines
+input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
@@ -12,7 +12,7 @@ if temperature_diff < 5
 elsif temperature_diff >= 5 && temperature_diff < 10 then
     # 処理
 elsif temperature_diff >= 10  then
-    # 処理
+    # 処理  
 end
 
 p require_time
